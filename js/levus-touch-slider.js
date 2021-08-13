dc3f2496d02e6848312a7b8905025158c3ddd295
lvivduncan <lvivduncan@gmail.com>
2021-08-13 15:15:15 +0300

    міша, всьо хуйня, давай па новай =(

--- js/levus-touch-slider.js
+++ js/levus-touch-slider.js
@@ -33,10 +33,10 @@ window.addEventListener('resize', debounce((e) => {
 
 
 const slider = document.querySelector('.levus-touch-slider .slides');
-// const slides = [...slider.querySelectorAll('.slide')];
 
 // на десктопі робимо налл, на мобільному нодлист
 let slides = slider.querySelectorAll('.slide');
+// let slides = [...slider.querySelectorAll('.slide')];
 
 // десктор -- скидаємо в налл
 // slides = null;
@@ -56,22 +56,21 @@ let last = '';
 // перший елемент
 let first = '';
 
-// зсув
-let shift = null; // maybe 0?
+// чекаємо на зсув
+let shift = 0;
+
+// встановлюємо параметри слайдера 
+const length = slides.length;
+const width = length * 100;
+const percent = 100 / length;
+
+// shift
+let transition = 0;
 
 // слайдер тільки у тому випадку, якщо слайди не налл (тільки на мобільному)
 if(slides !== null){
-
-    // встановлюємо параметри слайдера 
-    const length = slides.length;
-    const width = length * 100;
-
-    slider.style.cssText = `display: grid; 
-                            grid-template-columns: repeat(${length},1fr); 
-                            width: ${width}%; 
-                            cursor: grab; 
-                            position: relative;
-                            left: -100%`;
+                            
+    slider.style.cssText = `display: grid; grid-template-columns: repeat(${length},1fr); width: ${width}%; cursor: grab; margin-left: -100%`;
 
     slides.forEach(slide => {
         
@@ -81,7 +80,9 @@ if(slides !== null){
         slide.addEventListener('pointerdown', scrollStart);
         slide.addEventListener('pointermove', scrollMove);
         slide.addEventListener('pointerup', scrollEnd);
-        slide.addEventListener('pointerleave', scrollEnd);
+
+        // TODO: check mobile
+        // slide.addEventListener('pointerleave', scrollEnd);
         
     });
 }
@@ -103,70 +104,73 @@ function scrollMove(event){
         // місце, до якого тягнули
         finish = event.pageX;
 
-        if(finish - start <= 0){
+        // якщо тягнемо вліво
+        if(finish - start < 0){
 
             shift = finish - start - 20;
 
-            // якщо тягнемо вліво
-            slider.style.transform = `translateX(${shift}px)`;
 
-            last = slider.lastElementChild;
 
-            // clear
-            // first = null;
+            first = slider.firstElementChild;
+            // slider.append(first);
+            // slider.style.transitionX = `100%`;
         } 
         
+        // якщо тягнемо вправо
         if(finish - start > 0) { 
 
             shift = Math.abs(start - finish) + 20;
 
-            // якщо тягнемо вправо
-            slider.style.transform = `translateX(${shift}px)`;
 
-            first = slider.firstElementChild;
 
-            // clear
-            // last = null;
-
+            last = slider.lastElementChild;
+            slider.prepend(last); 
+            slider.style.transitionX = `-100%`;
         }
 
+        // slider.style.transform = `translateX(${shift}px)`;
+        slider.style.marginLeft = `${shift}px`;
     }
 }
 
-function scrollEnd(){
+function scrollEnd() {
 
-    // якщо тягнемо вправо (негатив) -- скролимо
-    if(finish - start <= 0){ 
+    // якщо тягнемо вліво
+    if(finish - start < 0){
 
-        slider.style.transform = `translateX(calc(100/${length}%))`;
+        // transition -= percent;
+        transition -= 100;
 
-        console.log(slider.style.transform)
-
-        setTimeout(() => {
-
-            slider.style.transform = 'translateX(0)';
-            // slider.style.transform = 'translateX(-25%)';
-            slider.prepend(last);
-        }, 500);
+        // first = slider.firstElementChild;
+        slider.append(first);
+        slider.style.transitionX = `100%`;
+        setTimeout(()=>{
+            slider.style.transitionX = `0`;
+        },500);
         
+        // slider.style.transitionX = `0`;
     }
+    
+    // якщо тягнемо вправо
+    if(finish - start > 0) {
 
-    // якщо тягнемо вліво (позитив) -- скролимо
-    if(finish - start > 0){
+        // transition += percent;
+        transition += 100;
 
-        slider.style.transform = `translateX(-100/${length}%)`;
-
-        console.log(slider.style.transform)
-
-        setTimeout( () => {
-
-            slider.style.transform = 'translateX(0)';
-            // slider.style.transform = 'translateX(25%)';
-            slider.append(first);
-        }, 500);
+        // last = slider.lastElementChild;
+        slider.prepend(last); 
+        slider.style.transitionX = `-100%`;
+        setTimeout(()=>{
+            slider.style.transitionX = `0`;
+        },500);
         
+        // slider.style.transitionX = `0`;
     }
+    
+    // slider.style.transform = `translateX(${transition}%)`;
+    slider.style.marginLeft = `${transition}%`;
 
+    // обнуляємо перевірку на перетягування
     drag = false;
     
     this.classList.remove('grabbing');
